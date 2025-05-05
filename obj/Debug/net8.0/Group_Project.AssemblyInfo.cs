08770ffdcdda8125841addd53f2a8439ade317fd
diep1cp <cdiep@usf.edu>
2025-05-05 11:57:37 -0500

    Add images and about.cshtml - Chi

--- obj/Debug/net8.0/Group_Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Group_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Group_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccb859b94dac77bcac30ec6f3c3713053497305b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Group_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Group_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
